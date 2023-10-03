cb4faf1df62b2d1e1acb65abfc61d9cd03169aba
miraballesramiro <124004198+miraballesramiro@users.noreply.github.com>
2023-10-03 00:00:51 -0300

    Add files via upload

---
+++ js/products.js
@@ -0,0 +1,30 @@
+const productos = [
+    {
+      id: 1,
+      nombre: "Harina",
+      precio: 50,
+      img: "https://i.blogs.es/95d4c3/harina-trigo-tipos/1366_2000.jpg",
+      cantidad: 1,
+    },
+    {
+      id: 2,
+      nombre: "Galletas",
+      precio: 100,
+      img: "https://mandolina.co/wp-content/uploads/2023/06/galletas-chispas-de-chocolate.jpg",
+      cantidad: 1,
+    },
+    {
+      id: 3,
+      nombre: "Cerveza",
+      precio: 150,
+      img: "https://www.rionegro.com.ar/wp-content/uploads/2022/12/cerveza-32424.png?w=1024",
+      cantidad: 1,
+    },
+    {
+      id: 4,
+      nombre: "Leche",
+      precio: 200,
+      img: "https://s3.abcstatics.com/media/bienestar/2020/07/09/leche-fucha-khMC--620x349@abc.jpg",
+      cantidad: 1,
+    },
+  ];
